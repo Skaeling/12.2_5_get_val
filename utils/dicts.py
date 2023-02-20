7e5c684b74dfef32b02961a5263544447be53bcd
Lana Virina <lana.virina@gmail.com>
2023-02-21 00:49:27 +0300

    Add get_val release

--- utils/dicts.py
+++ utils/dicts.py
@@ -2,4 +2,8 @@ def get_val(collection, key, default='git'):
     """Функция возвращает значение из словаря по переданному ключу,
     если ключ существует. В ином случае возвращается значение
     default"""
-    pass
+    for k, i in collection.items():
+        if k == key:
+            return i
+        else:
+            return default
